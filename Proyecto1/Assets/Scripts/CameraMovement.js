d01f68a36a2fead8ac463db0edc29132b2f65747
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2015-11-13 20:30:44 +0100

    CameraScript+PlayerScript(NotFinishedYet)

---
+++ Proyecto1/Assets/Scripts/CameraMovement.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private var player : Transform;
+public var posY : float = 2.0;
+public var despX: float = 2.0;
+
+function Start () {
+	player = GameObject.Find("Player").transform;
+	transform.position.y = posY;
+}
+
+function Update () {
+	transform.position = player.position - Vector3(despX, posY-player.position.y, -5.0f);
+}
