84ca2389ff5a05fff3cd5ec68ba15a8dd401e9a7
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2015-11-14 19:15:12 +0100

    PlayerMovement

--- Proyecto1/Assets/Scripts/CameraMovement.js
+++ Proyecto1/Assets/Scripts/CameraMovement.js
@@ -7,8 +7,10 @@ public var despX: float = 2.0;
 function Start () {
 	player = GameObject.Find("Player").transform;
 	transform.position.y = posY;
+	Debug.Log(transform.position);
 }
 
 function Update () {
 	transform.position = player.position - Vector3(despX, posY-player.position.y, -5.0f);
+	
 }
--- Proyecto1/Assets/Scripts/PlayerMovement.js
+++ Proyecto1/Assets/Scripts/PlayerMovement.js
@@ -1,17 +1,50 @@
 ﻿#pragma strict
 
+
+public var jumpspeed : float = 150;
+public var speed : float = 2.0;
+public var maxSpeed : float = 15.0;
+public var reduction : float = 1.5;
+
+private var floor: GameObject;
 private var cam : Transform;
+private var jumpClicked : boolean = false;
+private var actualJumpSpeed : float = 0.0;
+
 
 function Start () {
 	cam = GameObject.Find("Main Camera").transform;
 }
 
 function Update () {
+	/*
+	//Para probar sin el suelo
 	if (cam.position.y - transform.position.y > 8.0f) {
 		Application.LoadLevel(Application.loadedLevel);
 	}
+	*/
 }
 
 function FixedUpdate () {
-	GetComponent<Rigidbody2D>().AddForce(-Vector2.right*speed);
+	GetComponent.<Rigidbody2D>().AddForce(-Vector2.right*speed);
+	
+	if (GetComponent.<Rigidbody2D>().velocity.x >= maxSpeed) {
+		GetComponent.<Rigidbody2D>().velocity.x = maxSpeed;
+	}
+	
+	if (Input.GetButton("Jump")) {
+		jumpClicked = true;
+		actualJumpSpeed = jumpspeed;
+		
+		GetComponent.<Rigidbody2D>().AddForce(Vector2.up*actualJumpSpeed);
+		actualJumpSpeed -= reduction;
+	
+		if (jumpClicked) {
+			jumpClicked = false;
+			actualJumpSpeed = 0;
+			if (GetComponent.<Rigidbody2D>().velocity.y > 0 ) {
+				GetComponent.<Rigidbody2D>().velocity.y  = 1.0f;
+			}
+		}
+	}
 }
