45db6e55f74a9a44eea3c9cf6ed799c279dc0d50
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2015-11-15 18:08:40 +0100

    SomeNewSprites

---
+++ Proyecto1/Assets/Scripts/PickUp.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D (hit : Collider2D) {
+	if (hit.gameObject.tag == "Player") {
+		//GameObject.Find("Config").SendMessage("GetPickUp"); Para sumar los puntos en un FUTURO
+		//yield WaitForSeconds(2); // no usar el yiel en funciones update
+		Debug.Log(1);
+		Destroy(gameObject);
+	}
+}
