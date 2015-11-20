280c90c03a4704e6c1dfba63134d4b6990e722f6
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2015-11-20 17:10:52 +0100

    Pokeballs

---
+++ Proyecto1/Assets/Scripts/KillPlayer.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+private var anim:Animator;
+
+function Start () {
+	anim = GetComponent.<Animator>();
+	
+}
+
+function OnTriggerEnter2D (hit : Collider2D) {
+	if (hit.gameObject.tag == "Player") {
+		//GameObject.Find("Config").SendMessage("GetPickUp"); Para sumar los puntos en un FUTURO
+		//yield WaitForSeconds(2); // no usar el yiel en funciones update
+		//Debug.Log("Capturado!!!!!!!!!");
+		
+		anim.SetBool("PlayerFound", true);
+		yield WaitForSeconds(1);
+		//anim.SetBool("PlayerFound", false);
+		
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
