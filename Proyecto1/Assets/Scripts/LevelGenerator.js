145349c07e7f549b5648edfbd7c8904bbbfa7d51
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2015-11-20 19:10:38 +0100

    Add group of 1 to 4 berries

--- Proyecto1/Assets/Scripts/LevelGenerator.js
+++ Proyecto1/Assets/Scripts/LevelGenerator.js
@@ -35,20 +35,70 @@ function createNewBerries(LeftDownX : float , LeftDownY: float, RightDownX: floa
 	var startVec : Vector3 = Vector3(RightDownX, RightDownY, 0);
 	var platVec : Vector3 = Vector3(LeftDownX-RightDownX,LeftDownY-RightDownY,0);
 	var newPos : Vector3 = startVec+platVec*ScInitialPosition;
-	newPos.y+=Random.Range(1.0,14.0);
+	var offset : float = Random.Range(1.0,14.0);
+	newPos.y+=offset;
 	var newRot : Quaternion = Quaternion.identity;
-	Instantiate (berriePrefab, newPos,  newRot);
 	
+	var numBerries : int = Random.Range(0,5);
+	switch(numBerries){
+		case 0:
+			break;
+		case 1:
+			Instantiate (berriePrefab, newPos,  newRot);
+			break;
+		case 2:
+			Instantiate (berriePrefab, newPos,  newRot);
+			if(ScInitialPosition>5.0)
+				Instantiate (berriePrefab, newPos+Vector3(1.5,0,0),  newRot);
+			else Instantiate (berriePrefab, newPos+Vector3(-1.5,0,0),  newRot);
+			break;
+		case 3:
+			Instantiate (berriePrefab, newPos,  newRot);
+			if(ScInitialPosition>5.0){
+				Instantiate (berriePrefab, newPos+Vector3(1.5,0,0),  newRot);
+				if(offset>7.0) Instantiate (berriePrefab, newPos+Vector3(0.75,-1.5,0),  newRot);
+				else Instantiate (berriePrefab, newPos+Vector3(0.75,1.5,0),  newRot);
+			}
+			else{
+				Instantiate (berriePrefab, newPos+Vector3(-1.5,0,0),  newRot);
+				if(offset>7.0) Instantiate (berriePrefab, newPos+Vector3(-0.75,-1.5,0),  newRot);
+				else Instantiate (berriePrefab, newPos+Vector3(-0.75,1.5,0),  newRot);
+			}
+			
+		case 4:
+			Instantiate (berriePrefab, newPos,  newRot);
+			if(ScInitialPosition>5.0){
+				Instantiate (berriePrefab, newPos+Vector3(1.5,0,0),  newRot);
+				if(offset>7.0){
+					Instantiate (berriePrefab, newPos+Vector3(0,-1.5,0),  newRot);
+					Instantiate (berriePrefab, newPos+Vector3(1.5,-1.5,0),  newRot);
+					}
+				else{
+					Instantiate (berriePrefab, newPos+Vector3(0,1.5,0),  newRot);
+					Instantiate (berriePrefab, newPos+Vector3(1.5,1.5,0),  newRot);
+				}
+			}
+			else{
+				Instantiate (berriePrefab, newPos+Vector3(-1.5,0,0),  newRot);
+				if(offset>7.0){
+					Instantiate (berriePrefab, newPos+Vector3(0,-1.5,0),  newRot);
+					Instantiate (berriePrefab, newPos+Vector3(-1.5,-1.5,0),  newRot);
+					}
+				else{
+					Instantiate (berriePrefab, newPos+Vector3(0,1.5,0),  newRot);
+					Instantiate (berriePrefab, newPos+Vector3(-1.5,1.5,0),  newRot);
+				}
+			}
 	
-	
+	}
 }
 
 function getNewX(start : float){
 	return (start - Mathf.Abs(Mathf.Cos(newRotation.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2));
 }
 function getNewY(start : float){
-	if(newRotation.eulerAngles.z>180) return(start+ Mathf.Abs(Mathf.Sin(newRotation.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2));
-	else if(newRotation.eulerAngles.z<180) return(start - Mathf.Abs(Mathf.Sin(newRotation.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2));
+	if(newRotation.eulerAngles.z>=180) return(start+ Mathf.Abs(Mathf.Sin(newRotation.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2));
+	else return(start - Mathf.Abs(Mathf.Sin(newRotation.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2));
 }
 function getEndXofPlat(plat : Transform){
 	return plat.position.x - Mathf.Abs(Mathf.Cos(plat.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2);
@@ -56,8 +106,8 @@ function getEndXofPlat(plat : Transform){
 
 function getEndYofPlat(plat : Transform){
 	var ini : float;
-	if(plat.rotation.eulerAngles.z>180) return plat.position.y+Mathf.Abs(Mathf.Sin(plat.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2);
-	else if(plat.rotation.eulerAngles.z<180) return plat.position.y-Mathf.Abs(Mathf.Sin(plat.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2);	
+	if(plat.rotation.eulerAngles.z>=180) return plat.position.y+Mathf.Abs(Mathf.Sin(plat.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2);
+	else return plat.position.y-Mathf.Abs(Mathf.Sin(plat.eulerAngles.z*Mathf.Deg2Rad)*groundPrefab.transform.localScale.x/2);	
 }
 function getNewRandomRotation(){
 	newRotation = Quaternion.identity;
