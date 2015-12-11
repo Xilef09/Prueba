3ee64aff68ce1cac78a00a8fc22d871a8e317aff
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2015-12-11 17:38:13 +0100

    vereverec

--- Proyecto1/Assets/Scripts/KillPlayer.js
+++ Proyecto1/Assets/Scripts/KillPlayer.js
@@ -1,10 +1,27 @@
 ﻿#pragma strict
 
 private var anim:Animator;
+private var collisioned : boolean = false;
+private var time : float = 0.0;
+private var durationVelocity : float = 0.5;
+private var durationSize : float = 3.0;
 
 function Start () {
 	anim = GetComponent.<Animator>();
 	
+}
+
+function FixedUpdate (){
+		time+=Time.deltaTime;
+		
+		if (collisioned && time>durationVelocity){
+			 //Debug.Log(GetComponent.<Rigidbody2D>().velocity);
+			 GetComponent.<Rigidbody2D>().velocity=GetComponent.<Rigidbody2D>().velocity/1.5;
+			 //Debug.Log(GetComponent.<Rigidbody2D>().velocity);
+			 time = 0.0;
+		}
+		if (collisioned)
+			sizeChanger();
 }
 
 function OnCollisionEnter2D (hit : Collision2D) {
@@ -14,9 +31,15 @@ function OnCollisionEnter2D (hit : Collision2D) {
 		//Debug.Log("Capturado!!!!!!!!!");
 		
 		anim.SetBool("PlayerFound", true);
-		yield WaitForSeconds(50);
-		//anim.SetBool("PlayerFound", false);
-		
+		GetComponent.<Rigidbody2D>().gravityScale=0.0;
+		collisioned = true;
+		GetComponent.<Rigidbody2D>().velocity=GetComponent.<Rigidbody2D>().velocity/2;
+		yield WaitForSeconds(5);
+		//anim.SetBool("PlayerFound", false);		
 		Application.LoadLevel(Application.loadedLevel);
 	}
+
+}
+function sizeChanger(){
+	transform.localScale+=Vector3(Time.deltaTime/durationSize,Time.deltaTime/durationSize,Time.deltaTime/durationSize);
 }
